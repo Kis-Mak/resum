b033e5995e0327a34db6467953877aba6aac2c24
Makar Kislyakov <kislyakovma@miit.edu.mps>
2024-02-05 23:46:58 +0300

    js Output

--- js-cource/01_getElementById.js
+++ js-cource/01_getElementById.js
@@ -1,3 +1,11 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Вау, это действительно работает!";
 }
+
+function showTime() {
+    document.getElementById('time').innerHTML = Date();
+}
+
+function MyAlert() {
+    window.alert('Та самая птичка)')
+}
