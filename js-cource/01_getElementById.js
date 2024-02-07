ca3b22f46ea7bba3f32b20ffdaf974656f710be7
Makar Kislyakov <kislyakovma@miit.edu.mps>
2024-02-07 03:14:27 +0300

    js Syntax

--- js-cource/01_getElementById.js
+++ js-cource/01_getElementById.js
@@ -9,3 +9,13 @@ function showTime() {
 function MyAlert() {
     window.alert('Та самая птичка)')
 }
+
+$one = 'Это должно ';
+$two = 'прибавиться к этому ';
+$log = 'и отобразиться в логе';
+$result = $one + $two;
+
+console.log('Эта надпись должна отобразиться в f12 - console');
+console.log('Интересно, как выводить в лог переменную?!');
+console.log($result + $log);
+
